103d7e8653311078357ce0d24418325c58980b6d
cola <870252122@qq.com>
2018-09-28 14:47:28 +0800

    Initial commit

---
+++ cola-generator/src/main/java/com/yus/colagenerator/ColaGeneratorApplication.java
@@ -0,0 +1,12 @@
+package com.yus.colagenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ColaGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ColaGeneratorApplication.class, args);
+    }
+}
